ca46d62308c0eb748e3b82dd57285794f1f02798
Zhengyi Jiang <8778250+AnthonyZJiang@users.noreply.github.com>
2026-06-03 01:24:19 +0100

    improved example

--- example/capture_headless.py
+++ example/capture_headless.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python3
+from pathlib import Path
 
 from picamera2 import Picamera2
+
 
 picam2 = Picamera2()
 config = picam2.create_still_configuration()
@@ -9,7 +10,13 @@ picam2.configure(config)
 picam2.start()
 
 np_array = picam2.capture_array()
-print(np_array.shape)
-picam2.capture_file("/root/mnt/demo.jpg")
+print(f"Image shape: {np_array.shape}")
+
+image_dir = Path(__file__).parent / "mnt"
+
+if not image_dir.exists():
+    image_dir.mkdir(parents=True, exist_ok=True)
+    
+picam2.capture_file(image_dir / "demo.jpg")
 picam2.stop()
-print("Saved /root/mnt/demo.jpg")
+print(f"Saved {image_dir / 'demo.jpg'}")
